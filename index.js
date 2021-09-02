4957ee1654440823b0090e0b57ff5c949f340cdb
Harrison <gharrison3@una.edu>
2021-09-02 13:17:16 -0500

    changed display message

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 
-    res.send('<h3>Hello Node on Heroku</h3>');
+    res.send('<h1>Hello Node.js on Heroku</h1><p>by Harrison</p>');
 
 });
 
-app.get('/about', function(req, res){
+app.get('/about', function (req, res) {
 
-    res.sendFile('/README.md', {root: __dirname });
+    res.sendFile('/README.md', { root: __dirname });
 
 });
 
